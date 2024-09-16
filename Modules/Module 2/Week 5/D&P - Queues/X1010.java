25601e51fd096f58d795389150acda89a7f6d043
Alexander Prechtel <aprechtel0043@gmail.com>
2024-09-16 19:46:03 -0400

    fixed

--- Modules/Module 2/Week 5/D&P - Queues/X1010.java
+++ Modules/Module 2/Week 5/D&P - Queues/X1010.java
@@ -1,13 +1,11 @@
 void keepEvens(QueueADT<Integer> queue)
 {
     if(queue == null) return;
-    QueueADT<String> evens = new QueueADT<String>();
-    for(int i = 0; i < queue.numElements(); i++) {
+    int size = queue.numElements();
+    for(int i = 0; i < size; i++) {
         Integer value = queue.dequeue();
         if(value % 2 == 0){
-            evens.enqueue(value);
+            queue.enqueue(value);
         }
     }
-    queue = evens;
 }
-
