4fb12fb6b6b5bae9c691215514baea9bee1d0bc5
Alexander P <144269917+alexandernc0043@users.noreply.github.com>
2024-09-17 22:25:16 -0400

    Create X1337.java

---
+++ Modules/Module 1/Week 4/D&P - Comparing Objects/X1337.java
@@ -0,0 +1,18 @@
+  public class Name implements Comparable<Name> {
+    private String first;
+    private String last;
+    public Name (String f, String l) { ... }
+    public String getFirst() { ... }
+    public String getLast() { ... }
+    public String toString() { return getLast()+", "+getFirst(); }
+    public int compareTo(Name other) {
+      // to be completed
+    }
+  }
+public int compareTo(Name other)
+{
+    if(other == null) return -1;
+  else if(this.getLast().compareTo(other.getLast()) < 0) return -1;
+  else if(this.getLast().compareTo(other.getLast()) == 0) return 0;
+  else return 1;
+}
