ec0aaaea16c501f9c378fae4d324a8d36ab75896
Alexander P <144269917+alexandernc0043@users.noreply.github.com>
2024-09-17 22:24:31 -0400

    Create X1316.java

---
+++ Modules/Module 1/Week 4/D&P - Generics/X1316.java
@@ -0,0 +1,16 @@
+public int binarySearch(int[] numbers, int search)
+{
+   int low = 0;
+   int high = numbers.length-1;
+
+   while (low <= high) {
+      int mid = (low + high)/2;
+      if (search == numbers[mid])
+         return mid;  // found it
+      else if (search < numbers[mid])
+         high = mid - 1;
+      else
+         low = mid + 1;
+   }
+   return -1;  // not found
+}
