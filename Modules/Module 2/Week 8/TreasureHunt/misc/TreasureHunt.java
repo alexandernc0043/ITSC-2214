af9b1f973f67400e180af7b91eb603c5478b905c
desolationdev <aprechtel0043@gmail.com>
2024-10-11 19:23:11 -0400

    Class assignments

--- Modules/Module 2/Week 8/TreasureHunt/misc/TreasureHunt.java
+++ Modules/Module 2/Week 8/TreasureHunt/misc/TreasureHunt.java
@@ -1,14 +1,12 @@
 import java.util.function.Predicate;
 
 /**
- * TODO Write a one-sentence summary of your class here.
- * TODO Follow it with additional details about its purpose, what abstraction
- * it represents, and how to use it.
+ * Double Linked List Treasure Hunt Assignment.
  *
- * @author  TODO Your Name
+ * @author Alexander Prechtel
  * @version Sep 25, 2024
  */
-class LocationNode implements Comparable<LocationNode>{
+class LocationNode implements Comparable<LocationNode> {
     String name;
     boolean hasTreasure;
     private LocationNode next;
@@ -37,17 +35,18 @@ class LocationNode implements Comparable<LocationNode>{
 
     @Override
     public boolean equals(Object arg0) {
-        return this.name.equalsIgnoreCase(((LocationNode)arg0).getName()) 
+        return this.name.equalsIgnoreCase(((LocationNode) arg0).getName())
                 && this.hasTreasure == ((LocationNode) arg0).hasTreasure;
     }
 
     @Override
     public int compareTo(LocationNode arg0) {
         return this.name.compareToIgnoreCase(arg0.getName());
-     }
+    }
 
     /**
-     * Gets the node that follows this one 
+     * Gets the node that follows this one
+     * 
      * @return next linear node
      */
     public LocationNode getNext() {
@@ -56,14 +55,16 @@ class LocationNode implements Comparable<LocationNode>{
 
     /**
      * Sets the node that follows this one
+     * 
      * @param n LinearNode to follow this one
      */
     public void setNext(LocationNode n) {
         next = n;
     }
 
-    /** 
+    /**
      * Gets the node that is before this one
+     * 
      * @return prev LinearNode<T>
      */
     public LocationNode getPrev() {
@@ -72,6 +73,7 @@ class LocationNode implements Comparable<LocationNode>{
 
     /**
      * Sets the node that is before this one
+     * 
      * @param p LinearNode to put before this one
      */
     public void setPrev(LocationNode p) {
@@ -106,70 +108,91 @@ public class TreasureHunt {
     }
 
     /**
-     * Traverse the list starting from the head (starting point of the 
-     * treasure hunt) and print out the name of each location and whether 
-     * it contains a treasure. After reaching the last location (the tail), 
+     * Traverse the list starting from the head (starting point of the
+     * treasure hunt) and print out the name of each location and whether
+     * it contains a treasure. After reaching the last location (the tail),
      * traverse backward through the list to return to the starting point.
-     * Print in the following example format. For example, 
+     * Print in the following example format. For example,
      * Location [Y]->Riverbank [Y]->Abandoned Hut [N]->Riverbank [Y]->Forest [Y].
      * TODO Write statement for the following two cases:
-     *      - Do nothing, when the treasure map (doubly linked list) is empty
-     *      - When the treasure map is not empty
-     *          - Walk from the first location node through the second last node by using the 
-     *            ndex reference variable of each node
-     *          - Walk from the last node through the first node by using the 
-     *            prev reference variable of each node
+     * - Do nothing, when the treasure map (doubly linked list) is empty
+     * - When the treasure map is not empty
+     * - Walk from the first location node through the second last node by using the
+     * ndex reference variable of each node
+     * - Walk from the last node through the first node by using the
+     * prev reference variable of each node
      */
     public void traverse() {
-        //Do nothing, when the treasure map (doubly linked list) is empty
+        // Do nothing, when the treasure map (doubly linked list) is empty
         if (this.first == null)
             return;
-        if(this.last == null)
+        if (this.last == null)
             return;
-        //The treasure map is not empty
-        //Walk from the first location node through the second last node 
-        //by using the next reference variable of each node
+        // The treasure map is not empty
+        // Walk from the first location node through the second last node
+        // by using the next reference variable of each node
         LocationNode cur = this.first;
         while (cur.getNext() != null) {
-            System.out.print(cur.toString() + "->");
+            System.out.print(cur + "->");
             cur = cur.getNext();
         }
-        //TODO   Traverse from the last node through the 
-        //first node by using the prev reference variable 
-        //of each node and print. Refer to the previous while loop
-        cur = this.last;
-        while (cur.getPrev() != null) {
-            System.out.print(cur.toString() + "<-");
-            cur = cur.getPrev();
+
+        // TODO Traverse from the last node through the
+        // first node by using the prev reference variable
+        // of each node and print. Refer to the previous while loop
+        LocationNode last = this.last;
+        while (last != null) {
+            System.out.print(last.toString());
+            if(last.getPrev() != null){
+                System.out.print("<-");
+            }
+            last = last.getPrev();
         }
         System.out.println();
     }
 
     /**
-     * Insert a new location (node) in between two existing locations, 
+     * Insert a new location (node) in between two existing locations,
      * before and after.
-     * @param newLocation New location name
+     * 
+     * @param newLocation    New location name
      * @param newHasTreasure Whether this new location has treasure
-     * @param before The existing location after which the new location will
-     *               be inserted
-     * @param after The existing location before which the new location will
-     *               be inserted
+     * @param before         The existing location after which the new location will
+     *                       be inserted
+     * @param after          The existing location before which the new location
+     *                       will
+     *                       be inserted
      */
-    public void insert(String newLocation, boolean newHasTreasure,
-            String before, String after) {
-        //Do nothing when the treasure map has less than two locations or is null
-        if (this.first == null || this.first == this.last)
+    public void insert(String newLocation, boolean newHasTreasure, String before, String after) {
+        // Do nothing when the treasure map has less than two locations or is null
+        if (this.first == null || this.first == this.last) {
             return;
+        }
 
-        //TODO When the treasure map has two or more than two locations
-        //a) Walk through the treasure map and location the before and after locations
-        //b) Make a new location node and insert the new node between two existing locations
-
+        // TODO When the treasure map has two or more than two locations
+        // a) Walk through the treasure map and location the before and after locations
+        // b) Make a new location node and insert the new node between two existing
+        // locations
+        LocationNode current = first;
+        while (current != null && current.getNext() != null) {
+            if (current.getName().equals(before) && current.getNext().getName().equals(after)) {
+                LocationNode newNode = new LocationNode(newLocation, newHasTreasure);
+                LocationNode afterNode = current.getNext();
+                current.setNext(newNode);
+                newNode.setNext(afterNode);
+                newNode.setPrev(current);
+                afterNode.setPrev(newNode);
+                return;
+            } else {
+                current = current.getNext();
+            }
+        }
     }
-    
+
     /**
-     * Remove that nodes of having no treasures from the treasure 
+     * Remove that nodes of having no treasures from the treasure
      * map (doubly linked list).
+     * 
      * @param p Creteria that judge whether a location node satisfies
      */
     public void remove(Predicate<LocationNode> p) {
@@ -179,32 +202,53 @@ public class TreasureHunt {
         LocationNode cur = this.first;
         LocationNode pre = this.first;
         while (cur != null) {
-            if (p.test(cur)) {//True branch: locate the locationNode satifying the condition
-                //TODO Remove the identified node with the following three cases into consideration
-                // 1) If the identified node is the first node of the map, 
-                //    we need to remove this node and change the first reference variable to reflect the change after removing
-                // 2) If the identified node is the last node of the map, 
-                //    we need to remove this node and change the last reference variable to reflect the change after removing
-                // 3) If the identified node is the node somewhere in the middle of the map, 
-                //    we need to remove this node and hook up its previous node and its next node
+            if (p.test(cur)) {
+                // True branch: locate the locationNode satifying the condition
+                // TODO Remove the identified node with the following three cases into
+                // consideration
 
-            }else{
-                pre = cur;
+                // 1) If the identified node is the first node of the map,
+                // we need to remove this node and change the first reference variable
+                // to reflect the change after removing
+                
+                // 2) If the identified node is the last node of the map,
+                // we need to remove this node and change
+                // the last reference variable to reflect the change after removing
+                
+                // 3) If the identified node is the node somewhere in the middle of the map,
+                // we need to remove this node and hook up its previous node and its next node
+                if(cur == first){
+                    cur = cur.getNext();
+                    cur.setPrev(null);
+                    return;
+                }
+                if(cur == last){
+                    cur = cur.getPrev();
+                    cur.setNext(null);
+                }
+                else{
+                    cur.getPrev().setNext(cur.getNext());
+                    cur.getNext().setPrev(cur.getPrev());
+                }
+                return;
+
+            } else {
                 cur = cur.getNext();
+                pre = cur.getPrev();
             }
         }
     }
+
     public static void main(String[] argv) {
         TreasureHunt map = new TreasureHunt();
         map.addLast("Forest", true);
         map.addLast("Riverbank", true);
-        map.addLast("Abandoned Hut",false);
+        map.addLast("Abandoned Hut", false);
         map.addLast("Desert  of Wonders", true);
 
         map.traverse();
-        map.insert("Cave  of Wonders", true,
-                "Forest", "Riverbank");
-        
+        map.insert("Cave  of Wonders", true, "Forest", "Riverbank");
+
         System.out.println("After insertion, ");
         map.traverse();
 
