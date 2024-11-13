dc8d86bf8b76f40bd11eb18eb5733bfdd4f88927
desolationdev <aprechtel0043@gmail.com>
2024-11-13 00:13:44 -0500

    Renamed file

---
+++ Projects/Project-4/misc/App.java
@@ -0,0 +1,13 @@
+import itsc2214.*;
+
+/**
+ * App class, example for ITSC 2214 Course.
+ * 
+ * @author manuel a. perez-quinones
+ * @version July 2024
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
